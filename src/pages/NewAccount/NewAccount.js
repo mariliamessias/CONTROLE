2f7839373559a06bbdfc6d6426bbadb0feafc598
Marilia Aparecida Messias Oliveira <marilia.oliveira@terceiro-sky.com.br>
2019-07-18 12:33:11 -0300

    Fix form from newAccount

--- src/pages/NewAccount/NewAccount.js
+++ src/pages/NewAccount/NewAccount.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import Icon from '../../images/lontraIcon.png';
 import ButtonDefault from '../../components/Button/ButtonDefault';
 import InputText from '../../components/InputText/InputTex';
 import Button from 'react-bootstrap/Button';
@@ -17,41 +18,75 @@ class NewAccount extends Component {
       <div className="newAccount">
         <div className="newAccount-content">
 
-          <SimpleText className="newAccount-content-title">Criar Nova Conta</SimpleText>
+          <SimpleText className="newAccount-content-title">Crie sua conta!</SimpleText>
           <SimpleText className="newAccount-content-subtitle">Para você ter acesso ao sistema, precisamos de alguns dados seus para um simples cadastro. Para isso, preencha os campos abaixo conforme solicitado:</SimpleText>
-
-          <div className="newAccount-form">
-            <div className="newAccount-form-column-one">
-              <div className="newAccount-form-item">
-                <p className="newAccount-form-item-text">Gostaríamos muito de saber seu nome, informe para nós:</p>
-                <input className="form-control newAccount-form-item-input" />
+          <div className="newAccount-form-content">
+            <div className="newAccount-form-section1">
+              <div className="newAccount-form-icon">
+                <img className="newAccount-form-image" src={Icon} />
               </div>
+              <div className="newAccount-form">
               <div className="newAccount-form-item">
-                <p className="newAccount-form-item-text">Coloque o email que você mais utiliza:</p>
-                <input type="email" className="form-control newAccount-form-item-input" />
-              </div>
-              <div className="newAccount-form-item">
-                <p className="newAccount-form-item-text">Acreditamos em você, mas seria legal se você repetisse ele aqui:</p>
-                <input type="email" className="form-control newAccount-form-item-input" />
-              </div>
+              <p className="newAccount-form-item-text">Para te notificar e te ajudar a controlar suas despesas, informe para nós seu telefone celular:</p>
+                  <input className="form-control newAccount-form-item-input" />
+                </div>
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Coloque o email que você mais utiliza:</p>
+                  <input type="email" className="form-control newAccount-form-item-input" />
+                </div>
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Acreditamos em você, mas seria legal se você repetisse ele aqui:</p>
+                  <input type="email" className="form-control newAccount-form-item-input" />
+                </div>
             </div>
-            <div className="newAccount-form-column-two">
+             
+            </div> 
             </div>
-            <div className="newAccount-form-column-three">
-              <div className="newAccount-form-item">
-                <p className="newAccount-form-item-text">Para te notificar e te ajudar a controlar suas despesas, informe para nós seu telefone celular:</p>
-                <input className="form-control newAccount-form-item-input" />
-              </div>
-              <div className="newAccount-form-item">
-                <p className="newAccount-form-item-text">Indicamos utilizar uma senha forte, assim como você:</p>
-                <input type="password" className="form-control newAccount-form-item-input" />
-              </div>
-              <div className="newAccount-form-item">
-                <p className="newAccount-form-item-text">Só pra confirmar, repita ela aqui, por favor:</p>
-                <input type="password" className="form-control newAccount-form-item-input" />
-              </div>
-            </div>
+            <div className="newAccount-form-section2">
+            <div className="newAccount-form">
+                  <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Gostaríamos muito de saber seu nome, informe para nós:</p>
+                    <input className="form-control newAccount-form-item-input" />
+                  </div>
+                  <div className="newAccount-form-item">
+                    <p className="newAccount-form-item-text">Indicamos utilizar uma senha forte, assim como você:</p>
+                    <input type="password" className="form-control newAccount-form-item-input" />
+                  </div>
+                  <div className="newAccount-form-item">
+                    <p className="newAccount-form-item-text">Só pra confirmar, repita ela aqui, por favor:</p>
+                    <input type="password" className="form-control newAccount-form-item-input" />
+                  </div>
           </div>
+            {/* 
+            <div className="newAccount-form">
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Gostaríamos muito de saber seu nome, informe para nós:</p>
+                  <input className="form-control newAccount-form-item-input" />
+                </div>
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Coloque o email que você mais utiliza:</p>
+                  <input type="email" className="form-control newAccount-form-item-input" />
+                </div>
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Acreditamos em você, mas seria legal se você repetisse ele aqui:</p>
+                  <input type="email" className="form-control newAccount-form-item-input" />
+                </div>
+              
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Para te notificar e te ajudar a controlar suas despesas, informe para nós seu telefone celular:</p>
+                  <input className="form-control newAccount-form-item-input" />
+                </div>
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Indicamos utilizar uma senha forte, assim como você:</p>
+                  <input type="password" className="form-control newAccount-form-item-input" />
+                </div>
+                <div className="newAccount-form-item">
+                  <p className="newAccount-form-item-text">Só pra confirmar, repita ela aqui, por favor:</p>
+                  <input type="password" className="form-control newAccount-form-item-input" />
+                </div>
+              </div> */}
+          </div>
+    
           <div className="newAccount-form-buttons">
             <Link className="button-newAccount" to="/">Cancelar</Link>
             <Button className="button-newAccount">Confirmar</Button>
